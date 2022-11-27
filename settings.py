a56b0151b95555e847ceb42c7cf47e30149e3508
serg28290 <116976679+serg28290@users.noreply.github.com>
2022-11-27 17:23:43 +0500

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,1 +1,7 @@
-
+valid_email = "sergey28290@yandex.ru"
+valid_password = "VSJD9524"
+header = ('Access-Control-Allow-Origin', '*')
+methods = 'OPTIONS, POST, GET, HEAD'
+invalid_key = {'key': '12345'}
+max_age = '1'*1000000
+max_name = 'b'*1000000
